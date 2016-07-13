a36cb2fbe1f0e4bcbcac631dcce9c5f0dc587958
Arun <Arun@Arun-pc>
2016-07-13 11:04:56 +0530

    B-Browser: Ecafe: Opening the browsers for login page of ecafe projects

--- src/Configurations/BaseDriver.java
+++ src/Configurations/BaseDriver.java
@@ -1,5 +1,14 @@
 package Configurations;
 
-public class BaseDriver {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
 
+public class BaseDriver {
+	 public static WebDriver Driver=null;
+	 @BeforeSuite
+	 public static void Project_Config(){
+		 Driver=new FirefoxDriver();
+	 }
+	 
 }
